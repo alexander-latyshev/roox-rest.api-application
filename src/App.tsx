6deb017e54ae685eafec854294d83fc8fc6697fb
Alexander Latyshev <aladdelinxr@gmail.com>
2022-12-06 22:10:28 +0300

    webpack configured

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.scss";
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <h1>Done!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  devServer: {
+    port: 4000,
+    static: "./dist/",
+    hot: true,
+    historyApiFallback: true,
+    devMiddleware: {
+      publicPath: "/dist/",
+      writeToDisk: true,
+    },
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".scss"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(svg|png|gif|jpg)$/,
+        exclude: /fonts/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre",
+      },
+    ],
+  },
+};
