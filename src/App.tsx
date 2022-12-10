d90900a437d46019db5fe32758d38c1da8e3fae3
Alexander Latyshev <aladdelinxr@gmail.com>
2022-12-10 20:54:04 +0300

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import Main from "./components/main/main";
 import "./styles.scss";
 
 type Props = {};
 
 const App = (props: Props) => {
   return (
-    <div>
-      <h1>Done!</h1>
-    </div>
+    <>
+      <Main />
+    </>
   );
 };
 
